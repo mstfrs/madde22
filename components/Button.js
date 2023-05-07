c1f822a99b410f04013865084b0bbfae7d3d8324
mstfrs <mstfrs@gmail.com>
2023-05-07 18:21:21 +0300

    button adjusted

--- components/Button.js
+++ components/Button.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = () => {
   return (
-    <div className='bg-primary w-[147px] md:w-[120px] lg:w-[166px] md:h-[30px] lg:h-[48px] flex justify-center items-center cursor-pointer '>
+    <div className='transition-all hover:scale-95 bg-primary w-[147px] md:w-[120px] lg:w-[166px] md:h-[30px] lg:h-[48px] flex justify-center items-center cursor-pointer '>
         <p className='text-white font-bold leading-7 text-sm lg:text-base'>Bilet Al</p>
 
     </div>
